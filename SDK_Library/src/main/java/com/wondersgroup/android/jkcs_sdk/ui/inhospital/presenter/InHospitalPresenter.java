4128e8dc762e24bed51d52bd5a1c2d77c8f02c02
xinpengfei <542270559@qq.com>
2018-11-07 10:36:20 +0800

    in hospital page.

---
+++ SDK_Library/src/main/java/com/wondersgroup/android/jkcs_sdk/ui/inhospital/presenter/InHospitalPresenter.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.wondersgroup.android.jkcs_sdk.ui.inhospital.presenter;
+
+import com.wondersgroup.android.jkcs_sdk.base.MvpBasePresenter;
+import com.wondersgroup.android.jkcs_sdk.ui.inhospital.contract.InHospitalContract;
+import com.wondersgroup.android.jkcs_sdk.ui.inhospital.model.InHospitalModel;
+
+/**
+ * Created by x-sir on 2018/11/7 :)
+ * Function:住院页面的 Presenter
+ */
+public class InHospitalPresenter<T extends InHospitalContract.IView>
+        extends MvpBasePresenter<T> implements InHospitalContract.IPresenter {
+
+    private static final String TAG = "InHospitalPresenter";
+    private InHospitalContract.IModel mModel = new InHospitalModel();
+
+}
