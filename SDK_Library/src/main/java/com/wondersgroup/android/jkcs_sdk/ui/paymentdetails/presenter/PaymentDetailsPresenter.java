c7b73eb279702490bacda00569adc62d915dd37d
xinpengfei <542270559@qq.com>
2018-11-22 20:58:59 +0800

    处理后台异步结算中的状态

--- SDK_Library/src/main/java/com/wondersgroup/android/jkcs_sdk/ui/paymentdetails/presenter/PaymentDetailsPresenter.java
+++ SDK_Library/src/main/java/com/wondersgroup/android/jkcs_sdk/ui/paymentdetails/presenter/PaymentDetailsPresenter.java
@@ -213,8 +213,11 @@ public class PaymentDetailsPresenter<T extends PaymentDetailsContract.IView>
                     dismissLoading();
                     WToastUtil.show(errCodeDes);
                     if (isNonNull()) {
-                        // 传 null 表示正式结算失败！
-                        mViewRef.get().onOfficialSettleResult(null);
+                        // 处理后台异步结算中的状态
+                        if (!"程序异步正式结算中".equals(errCodeDes)) {
+                            // 传 null 表示正式结算失败！
+                            mViewRef.get().onOfficialSettleResult(null);
+                        }
                     }
                 }
             });
