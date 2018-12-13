40e193a473b53612f1b855c8fa1a7eef406c86a2
xinpengfei <542270559@qq.com>
2018-12-13 11:18:34 +0800

    initialize person list.

--- app/src/main/java/com/wondersgroup/android/healthcity_sdk/MainActivity.java
+++ app/src/main/java/com/wondersgroup/android/healthcity_sdk/MainActivity.java
@@ -14,11 +14,33 @@ import com.wondersgroup.android.healthcity_sdk.utils.AppInfoUtil;
 import com.wondersgroup.android.jkcs_sdk.entity.UserBuilder;
 import com.wondersgroup.android.jkcs_sdk.utils.WondersGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] NAME = {"吴振强", "陆晓明", "朱凯", "潘人伟", "杨旭成", "沈佳威", "赵岳寅", "钟瑜", "唐其儿", "邱晨飞", "钟央毛", "程华凤", "测试2", "严超"};
+    private static final String[] ID_NUM = {"330501199008213715", "330502197207121415", "330501199005222018", "330502196310210630", "330102197703011512", "330501198709151313", "330681198610031570", "330501198112238539", "330501198908158673", "330501199001262215", "330511193603094218", "330523196701243028", "330501198804146213", "330501198804146213", "000000000"};
+    private static final String[] CARD_TYPE = {"0", "0", "0", "0", "0", "0", "0", "0", "0", "2", "0", "0", "0", "0"};
+    private static final String[] CARD_NUM = {"A05111650", "A0109403X", "A0486807X", "A00305253", "A00997691", "A02418702", "A05401164", "A03111112", "A05300523", "006953059", "000000000", "000000000", "000000000", "000000000"};
+    /**
+     * 证件类型 01 身份证
+     */
+    private static final String ID_TYPE = "01";
+    /**
+     * 手机号码
+     */
+    private static final String PHONE = "13588259873";
+    /**
+     * 家庭住址
+     */
+    private static final String ADDRESS = "ShangHai";
+
+    private List<PersonBean> mPersonList = new ArrayList<>();
 
     @BindView(R.id.etName)
     EditText etName;
@@ -66,22 +88,10 @@ public class MainActivity extends AppCompatActivity {
     TextView tvMrCheng;
     @BindView(R.id.tvMrTest2)
     TextView tvMrTest2;
+    @BindView(R.id.tvMrYanChao)
+    TextView tvMrYanChao;
     @BindView(R.id.tvVersion)
     TextView tvVersion;
-
-    private PersonBean mPersonWu;
-    private PersonBean mPersonLu;
-    private PersonBean mPersonZhu;
-    private PersonBean mPersonPan;
-    private PersonBean mPersonYang;
-    private PersonBean mPersonShen;
-    private PersonBean mPersonZhao;
-    private PersonBean mPersonZhong;
-    private PersonBean mPersonTang;
-    private PersonBean mPersonQiu;
-    private PersonBean mPersonZym;
-    private PersonBean mPersonCheng;
-    private PersonBean mPersonTest2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,181 +107,69 @@ public class MainActivity extends AppCompatActivity {
         String version = AppInfoUtil.getVersionName(this);
         String versionName = "demo 版本：V" + version;
         tvVersion.setText(versionName);
+        initPerson();
+    }
 
-        /*吴振强 的就是专门测试登录注册这些的*/
-        mPersonWu = new PersonBean();
-        mPersonWu.setName("吴振强");
-        mPersonWu.setPhone("13588259873");
-        mPersonWu.setIdType("01");
-        mPersonWu.setIdNum("330501199008213715");
-        mPersonWu.setCardType("0");
-        mPersonWu.setCardNum("A05111650");
-        mPersonWu.setAddress("ShangHai");
-
-        /*第一医院 陆晓明 主要测试账单欠费这些*/
-        mPersonLu = new PersonBean();
-        mPersonLu.setName("陆晓明");
-        mPersonLu.setPhone("13588259873");
-        mPersonLu.setIdType("01");
-        mPersonLu.setIdNum("330502197207121415");
-        mPersonLu.setCardType("0");
-        mPersonLu.setCardNum("A0109403X");
-        mPersonLu.setAddress("ShangHai");
-
-        /*中心医院 朱凯 主要测试账单等信息*/
-        mPersonZhu = new PersonBean();
-        mPersonZhu.setName("朱凯");
-        mPersonZhu.setPhone("13588259873");
-        mPersonZhu.setIdType("01");
-        mPersonZhu.setIdNum("330501199005222018");
-        mPersonZhu.setCardType("0");
-        mPersonZhu.setCardNum("A0486807X");
-        mPersonZhu.setAddress("ShangHai");
-
-        /*中心医院 潘人伟 主要测试账单等信息*/
-        mPersonPan = new PersonBean();
-        mPersonPan.setName("潘人伟");
-        mPersonPan.setPhone("13588259873");
-        mPersonPan.setIdType("01");
-        mPersonPan.setIdNum("330502196310210630");
-        mPersonPan.setCardType("0");
-        mPersonPan.setCardNum("A00305253");
-        mPersonPan.setAddress("ShangHai");
-
-        /*中心医院 杨旭成 主要测试账单等信息*/
-        mPersonYang = new PersonBean();
-        mPersonYang.setName("杨旭成");
-        mPersonYang.setPhone("13588259873");
-        mPersonYang.setIdType("01");
-        mPersonYang.setIdNum("330102197703011512");
-        mPersonYang.setCardType("0");
-        mPersonYang.setCardNum("A00997691");
-        mPersonYang.setAddress("ShangHai");
-
-        /*中心医院 沈佳威 主要测试账单等信息*/
-        mPersonShen = new PersonBean();
-        mPersonShen.setName("沈佳威");
-        mPersonShen.setPhone("13588259873");
-        mPersonShen.setIdType("01");
-        mPersonShen.setIdNum("330501198709151313");
-        mPersonShen.setCardType("0");
-        mPersonShen.setCardNum("A02418702");
-        mPersonShen.setAddress("ShangHai");
-
-        /*中心医院 赵岳寅 主要测试账单等信息*/
-        mPersonZhao = new PersonBean();
-        mPersonZhao.setName("赵岳寅");
-        mPersonZhao.setPhone("13588259873");
-        mPersonZhao.setIdType("01");
-        mPersonZhao.setIdNum("330681198610031570");
-        mPersonZhao.setCardType("0");
-        mPersonZhao.setCardNum("A05401164");
-        mPersonZhao.setAddress("ShangHai");
-
-        /*中心医院 钟瑜 主要测试账单等信息*/
-        mPersonZhong = new PersonBean();
-        mPersonZhong.setName("钟瑜");
-        mPersonZhong.setPhone("13588259873");
-        mPersonZhong.setIdType("01");
-        mPersonZhong.setIdNum("330501198112238539");
-        mPersonZhong.setCardType("0");
-        mPersonZhong.setCardNum("A03111112");
-        mPersonZhong.setAddress("ShangHai");
-
-        /*唐其儿*/
-        mPersonTang = new PersonBean();
-        mPersonTang.setName("唐其儿");
-        mPersonTang.setPhone("13588259873");
-        mPersonTang.setIdType("01");
-        mPersonTang.setIdNum("330501198908158673");
-        mPersonTang.setCardType("0");
-        mPersonTang.setCardNum("A05300523");
-        mPersonTang.setAddress("ShangHai");
-
-        /*三院资自费卡邱晨飞*/
-        mPersonQiu = new PersonBean();
-        mPersonQiu.setName("邱晨飞");
-        mPersonQiu.setPhone("13588259873");
-        mPersonQiu.setIdType("01");
-        mPersonQiu.setIdNum("330501199001262215");
-        mPersonQiu.setCardType("2");
-        mPersonQiu.setCardNum("006953059");
-        mPersonQiu.setAddress("ShangHai");
-
-        /*三院住院钟央毛*/
-        mPersonZym = new PersonBean();
-        mPersonZym.setName("钟央毛");
-        mPersonZym.setPhone("13588259873");
-        mPersonZym.setIdType("01");
-        mPersonZym.setIdNum("330511193603094218");
-        mPersonZym.setCardType("0");
-        mPersonZym.setCardNum("000000000");
-        mPersonZym.setAddress("ShangHai");
-
-        /*中心医院住院程华凤*/
-        mPersonCheng = new PersonBean();
-        mPersonCheng.setName("程华凤");
-        mPersonCheng.setPhone("13588259873");
-        mPersonCheng.setIdType("01");
-        mPersonCheng.setIdNum("330523196701243028");
-        mPersonCheng.setCardType("0");
-        mPersonCheng.setCardNum("000000000");
-        mPersonCheng.setAddress("ShangHai");
-
-        /*中心医院住院测试2*/
-        mPersonTest2 = new PersonBean();
-        mPersonTest2.setName("测试2");
-        mPersonTest2.setPhone("13588259873");
-        mPersonTest2.setIdType("01");
-        mPersonTest2.setIdNum("330501198804146213");
-        mPersonTest2.setCardType("0");
-        mPersonTest2.setCardNum("000000000");
-        mPersonTest2.setAddress("ShangHai");
+    private void initPerson() {
+        for (int i = 0; i < NAME.length; i++) {
+            PersonBean personBean = new PersonBean();
+            personBean.setName(NAME[i]);
+            personBean.setPhone(PHONE);
+            personBean.setIdType(ID_TYPE);
+            personBean.setIdNum(ID_NUM[i]);
+            personBean.setCardType(CARD_TYPE[i]);
+            personBean.setCardNum(CARD_NUM[i]);
+            personBean.setAddress(ADDRESS);
+            mPersonList.add(personBean);
+        }
     }
 
     @OnClick({R.id.tvMrWu, R.id.tvMrLu, R.id.tvMrZhu, R.id.tvMrPan, R.id.tvMrYang, R.id.tvMrShen,
             R.id.tvMrZhao, R.id.tvMrZhong, R.id.tvMrTang, R.id.tvMrQiu, R.id.tvMrZym, R.id.tvMrCheng,
-            R.id.tvMrTest2,R.id.btnAfterPayHome, R.id.btnSelfPayHome, R.id.btnInHospitalHome})
+            R.id.tvMrTest2, R.id.tvMrYanChao, R.id.btnAfterPayHome, R.id.btnSelfPayHome, R.id.btnInHospitalHome})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tvMrWu:
-                setPersonInfo(mPersonWu);
+                setPersonInfo(mPersonList.get(0));
                 break;
             case R.id.tvMrLu:
-                setPersonInfo(mPersonLu);
+                setPersonInfo(mPersonList.get(1));
                 break;
             case R.id.tvMrZhu:
-                setPersonInfo(mPersonZhu);
+                setPersonInfo(mPersonList.get(2));
                 break;
             case R.id.tvMrPan:
-                setPersonInfo(mPersonPan);
+                setPersonInfo(mPersonList.get(3));
                 break;
             case R.id.tvMrYang:
-                setPersonInfo(mPersonYang);
+                setPersonInfo(mPersonList.get(4));
                 break;
             case R.id.tvMrShen:
-                setPersonInfo(mPersonShen);
+                setPersonInfo(mPersonList.get(5));
                 break;
             case R.id.tvMrZhao:
-                setPersonInfo(mPersonZhao);
+                setPersonInfo(mPersonList.get(6));
                 break;
             case R.id.tvMrZhong:
-                setPersonInfo(mPersonZhong);
+                setPersonInfo(mPersonList.get(7));
                 break;
             case R.id.tvMrTang:
-                setPersonInfo(mPersonTang);
+                setPersonInfo(mPersonList.get(8));
                 break;
             case R.id.tvMrQiu:
-                setPersonInfo(mPersonQiu);
+                setPersonInfo(mPersonList.get(9));
                 break;
             case R.id.tvMrZym:
-                setPersonInfo(mPersonZym);
+                setPersonInfo(mPersonList.get(10));
                 break;
             case R.id.tvMrCheng:
-                setPersonInfo(mPersonCheng);
+                setPersonInfo(mPersonList.get(11));
                 break;
             case R.id.tvMrTest2:
-                setPersonInfo(mPersonTest2);
+                setPersonInfo(mPersonList.get(12));
+                break;
+            case R.id.tvMrYanChao:
+                setPersonInfo(mPersonList.get(13));
                 break;
             case R.id.btnAfterPayHome:
                 startBusiness(0);
