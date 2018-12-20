2a6e596c29ad803cd98177f91f57a80efdbac910
xinpengfei <542270559@qq.com>
2018-12-20 10:24:49 +0800

    empty view.

--- SDK_Library/src/main/java/com/wondersgroup/android/jkcs_sdk/ui/inhospitalhistory/view/InHospitalHistory.java
+++ SDK_Library/src/main/java/com/wondersgroup/android/jkcs_sdk/ui/inhospitalhistory/view/InHospitalHistory.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -181,6 +182,8 @@ public class InHospitalHistory extends MvpBaseActivity<InHosHisContract.IView,
 
     private void setAdapter() {
         mHosHistoryAdapter = new HosHistoryAdapter(R.layout.wonders_group_in_hos_record_item, mDetails);
+        View notDataView = getLayoutInflater().inflate(R.layout.wonders_group_empty_view, (ViewGroup) recyclerView.getParent(), false);
+        mHosHistoryAdapter.setEmptyView(notDataView);
         mHosHistoryAdapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
         mHosHistoryAdapter.isFirstOnly(false);
         recyclerView.setAdapter(mHosHistoryAdapter);
