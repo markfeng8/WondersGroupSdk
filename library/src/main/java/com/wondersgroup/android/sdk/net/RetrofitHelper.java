d2059c9c6f13a559882652db5759497ed151ea8f
Administrator <1246114057@qq.com>
2021-06-17 16:08:34 +0800

    修改接口请求时间

--- library/src/main/java/com/wondersgroup/android/sdk/net/RetrofitHelper.java
+++ library/src/main/java/com/wondersgroup/android/sdk/net/RetrofitHelper.java
@@ -23,7 +23,7 @@ public class RetrofitHelper {
 
     private static final String TAG = "RetrofitHelper";
     private static final String BASE_URL = RequestUrl.HOST;
-    private static final long DEFAULT_TIMEOUT = 60000L;
+    private static final long DEFAULT_TIMEOUT = 60L;
     private Retrofit mRetrofit;
 
     public static RetrofitHelper getInstance() {
