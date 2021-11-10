0fb495e02462a546e99c471a0c426cf19cb4b16f
fengchuantian <fengchuantian@wondersgroup.com>
2021-11-10 10:42:45 +0800

    提交

--- library/src/main/java/com/wondersgroup/android/sdk/net/callback/ApiSubscriber.java
+++ library/src/main/java/com/wondersgroup/android/sdk/net/callback/ApiSubscriber.java
@@ -59,9 +59,10 @@ public class ApiSubscriber<T, E> extends ResourceSubscriber<T> {
                     } else {
                         String errCodeDes = baseEntity.getErrCodeDes();
                         if (!TextUtils.isEmpty(errCodeDes)) {
-                            if (mCallback != null) {
-                                mCallback.onFailed(errCodeDes);
-                            }
+                            errCodeDes = " ";
+                        }
+                        if (mCallback != null) {
+                            mCallback.onFailed(errCodeDes);
                         }
                     }
                 }
